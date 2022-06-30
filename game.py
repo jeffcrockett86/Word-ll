5bcf680f909a38d6f5ea793f89c57059da966402
Jeff <jeffcrockett03@gmail.com>
2022-06-30 19:54:08 -0400

    commented out play function and call to it

--- game.py
+++ game.py
@@ -20,25 +20,25 @@ def orange_and_green(word):
     output = []
     for i in range(len(word) - 1):
         output.append((set(words_with(word[i])), set(words_with(word[i+1]))))
-        output = output[-1][0] & output[-1][1]
-        print('output[index] is', output[i])
+        output = set(output[-1][0] & output[-1][1])
+        print('output[index] is', list(output)[i])
 
     return output
 
 
-def play(word, guess_num, words=words):
-    guess_num += 1
-    print('word is', word)
-    print('guess num is', guess_num)
-    # answer = 'hello'
-    # print(answer)
-    if guess_num > 4 or len(word) == 0:
-        return {}
-    sys.argv = sys.argv[1:]
-    print(sys.argv)
-    print('word is', word)
-    return set(word[0]) & set(list(play(word[1:], guess_num, words_with(word[0]))))
-    # return set([play(letter, num_guesses=num_guesses) for letter, i in word[] if letter in word and letter in answer])
+# def play(word, guess_num, words=words):
+#     guess_num += 1
+#     print('word is', word)
+#     print('guess num is', guess_num)
+#     # answer = 'hello'
+#     # print(answer)
+#     if guess_num > 4 or len(word) == 0:
+#         return {}
+#     sys.argv = sys.argv[1:]
+#     print(sys.argv)
+#     print('word is', word)
+#     return set(word[0]) & set(list(play(word[1:], guess_num, words_with(word[0]))))
+#     # return set([play(letter, num_guesses=num_guesses) for letter, i in word[] if letter in word and letter in answer])
 
 
-print('words with slant', play('holho', 0))
+# print('words with slant', play('holho', 0))
