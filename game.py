78e5cbab61322f09601154c35f22ebed3555dcf8
Jeff <jeffcrockett03@gmail.com>
2022-06-30 19:45:48 -0400

    added orange_and_green(word)

--- game.py
+++ game.py
@@ -1,107 +1,44 @@
+import sys
 import random
-import termcolor
-import re
 
-#open the file
-f = open('wordleAlpha.txt')
-words = f.read().split('\n')
-#turn the file's text into a string
+"""
+>>> play('a') & play('p') & play('l') & play('e')
+{'plane', 'pearl', 'leapt', 'place', 'penal', 'maple', 'lapse', 'plate',
+'ample', 'paler', 'pedal', 'apple', 'pleat', 'panel', 'plead', 'petal', 'lapel'}
+"""
+
+f = open('wordleAlpha.txt', 'r')
+sys.argv = ['slant', 'guest', 'reach', 'whirl']
+words = [word for word in f.read().split('\n')][:-1]
+answer = 'hello'
+
+def words_with(letter):
+    return [word for word in words if letter in word and letter in answer]
 
 
-# print(text)
+def orange_and_green(word):
+    output = []
+    for i in range(len(word) - 1):
+        output.append((set(words_with(word[i])), set(words_with(word[i+1]))))
+        output = output[-1][0] & output[-1][1]
+        print('output[index] is', output[i])
 
-#generate 5 letter words from the text file
+    return output
 
-row = ['_', '_', '_', '_', '_']
-# print(words)
-#answer is random word from words
-answer = words[random.randint(0, len(words) - 1)]
 
-def display_instructions():
-    print(termcolor. colored('---', 'red') + ' means wrong letter wrong place\n')
-    print(termcolor. colored('---', 'white') + ' means wrong letter wrong place\n')
-    print(termcolor. colored('---', 'yellow') + ' means right letter wrong place\n')
+def play(word, guess_num, words=words):
+    guess_num += 1
+    print('word is', word)
+    print('guess num is', guess_num)
+    # answer = 'hello'
+    # print(answer)
+    if guess_num > 4 or len(word) == 0:
+        return {}
+    sys.argv = sys.argv[1:]
+    print(sys.argv)
+    print('word is', word)
+    return set(word[0]) & set(list(play(word[1:], guess_num, words_with(word[0]))))
+    # return set([play(letter, num_guesses=num_guesses) for letter, i in word[] if letter in word and letter in answer])
 
-def valid(guess):
-   if re.match(r'^[A-Za-z]{5}$', guess):
-     return True
-   else:
-     return False
-#make sure guess's length is 5 and it only has letters
-# def validate_and_check(guess):
-#     if re.match(f'^[a-zA-Z]{5}$', guess):
-#         for letter in word:
-#             check(letter)
-#     else:
-#        print('asdf')
 
-# validate = guess: re.match(r'^[A-Za-z]{5}$', guess)
-
-#check returns the letter but with the right color
-def check(guess, guess_count=0):
-    if guess == answer:
-        print(termcolor.colored(answer, 'green'))
-        print("You win!")
-        return
-    if not valid(guess):
-        print('Invalid guess')
-        guess = input(' '.join(row))
-        check(guess)
-    while guess_count < 6:
-        guess_count +=1
-        print('answer is', answer)
-        print('guess count is', guess_count)
-        for i in range(len(guess)):
-            # print(guess[i])
-            #if right letter right place
-            if guess[i] == answer[i]:
-                row[i] = termcolor.colored(guess[i], 'green')
-
-        #if right letter wrong place
-            elif guess[i] in answer and guess[i] != answer[i]:
-                row[i] = termcolor.colored(guess[i], 'yellow')
-            else:
-                continue
-
-        guess = input(''.join(row) + '\n\n')
-        # print('guess is', guess)
-        # guess_count += 1
-        print('guess count is', guess_count)
-        guess = str(guess[:5])
-        # if guess == answer:
-        #     print(termcolor.colored(guess, 'green'))
-        #     print("You win!")
-        #     return
-        check(guess, guess_count)
-
-    # if right_letter_wrong_place(letter):
-    #     return input([termcolor.colored(letter, 'yellow')] + [check(each_letter) for each_letter in guess])#]]termcolor. colored('---', 'red') + ' means wrong letter wrong place') +
-    # if right_letter_right_place(letter):
-    #     return input(termcolor.colored(letter, 'yellow'))
-    # else:
-    #     return input(termcolor.colored(letter, 'white'))
-
-#display the instructions
-def display_instructions():
-    print(termcolor. colored('---', 'red') + ' means wrong letter wrong place')
-    print(termcolor. colored('---', 'white') + ' means wrong letter wrong place')
-    print(termcolor. colored('---', 'yellow') + ' means right letter wrong place')
-inp = input("Welcome to Word'll! Press x to see instructions. Press any other key to begin.\n\n")
-if inp.lower() == 'x':
-    display_instructions()
-
-else:
-    guess = input(' '.join(row) + '\n\n')
-    # while(guess != answer):
-
-    # if valid(guess):
-    check(guess)
-
-# answer = 'apple'
-# guess = 'azzzz'
-# print(guess)
-# while(guess != answer):
-#     validate(guess)
-#     break
-# print('You win!')
-# check([letter for letter in guess]]
+print('words with slant', play('holho', 0))
