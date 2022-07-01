3ea3311c0eb0339462bbc9e44b5f7075b46ee3e5
Jeff <jeffcrockett03@gmail.com>
2022-06-30 21:36:19 -0400

    updated words_with

--- game.py
+++ game.py
@@ -7,21 +7,31 @@ import random
 'ample', 'paler', 'pedal', 'apple', 'pleat', 'panel', 'plead', 'petal', 'lapel'}
 """
 
+"""
+from game import *
+oag=orange_and_green
+oag('white')
+"""
+
 f = open('wordleAlpha.txt', 'r')
 sys.argv = ['slant', 'guest', 'reach', 'whirl']
 words = [word for word in f.read().split('\n')][:-1]
 answer = 'hello'
 
-def words_with(letter):
+def words_with(letter, words=words):
     return [word for word in words if letter in word and letter in answer]
 
 
 def orange_and_green(word):
     output = []
     for i in range(len(word) - 1):
-        output.append((set(words_with(word[i])), set(words_with(word[i+1]))))
-        output = set(output[-1][0] & output[-1][1])
-        print('output[index] is', list(output)[i])
+        set1 = set(words_with(word[i]))
+        set2 = set(words_with(word[i+1]))
+        output.append((set1, set2))
+        print(output[-1])
+        # output = list(set(output[-1][0] & output[-1][1]))
+        output.append(word[i])
+        # print('output[index] is', output[i])
 
     return output
 
