1372a5942a435d3b1dd88f3cc5d6c161bd7f8278
Jeff <jeffcrockett03@gmail.com>
2022-07-01 11:29:38 -0400

    added top comment lmao

--- game.py
+++ game.py
@@ -9,6 +9,19 @@ f = open('wordleAlpha.txt', 'r')
 sys.argv = ['slant', 'guest', 'reach', 'whirl']
 words = [word for word in f.read().split('\n')][:-1]
 answer = 'hello'
+"""
+>>> [j for j in set(func('e'))]
+['eagle', 'event', 'elbow', 'ethic', 'eying', 'elate', 'enjoy',
+'enter', 'evict', 'elude', 'expel', 'ember', 'early', 'envoy',
+'epoxy', 'erase', 'error', 'erupt', 'elegy', 'embed', 'eking', 'elide',
+'ethos', 'epoch', 'evade', 'every', 'exist', 'eater', 'exact', 'earth',
+'endow', 'enemy', 'exult', 'ether', 'ensue', 'exalt', 'extol', 'eclat',
+'equip', 'elect', 'eager', 'entry', 'edify', 'empty', 'edict', 'egret',
+'ebony', 'elfin', 'elope', 'ester', 'eerie', 'eject', 'evoke', 'erect',
+'extra', 'elder', 'enema', 'enact', 'emcee', 'ennui', 'excel', 'exile',
+'equal', 'easel', 'email', 'erode', 'etude', 'eight', 'exert', 'eaten',
+'essay', 'elite']
+"""
 def func(letter, words=words):
     return list(filter(lambda x: x[0] == letter, words))
 
@@ -37,6 +50,7 @@ pairs = [x[0] for x in g if len(x) > 0]
 new_words = [pair[0] for pair in pairs]
 dups = set([x[0] for x in new_words])
 q = lambda x: [func(x) for x in list(dups)][2]
+x = [q(letter) for letter in dups]
 # print(set([x[0] for x in [set(new_words[i]) for i in range(len(new_words))]]))
 
 # new_words_beta =
